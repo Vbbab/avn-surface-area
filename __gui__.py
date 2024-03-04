5e8378fcba261d49243f43d8f77769f8c57545b7
Chaaruhaas Kandregula <chaaskandregula@gmail.com>
2024-03-03 23:11:45 -0800

    __gui__ has an annotator with rng; image updater still not cooperating with slider; that is to be finished tomorrow

--- __gui__.py
+++ __gui__.py
@@ -6,6 +6,7 @@ from tkinter import filedialog as filedialog
 import matplotlib.pyplot as plt
 import numpy as np
 from PIL import Image, ImageTk
+import random
 
 root = tk.Tk()
 root.geometry('1000x1000')
@@ -21,16 +22,10 @@ def diruploader():
     directory = filedialog.askdirectory(master=root)
     return directory
 
+def annotate():
+    annotation=str(random.randint(100,999))
+    canvas.itemconfig(annotation_text, text=annotation)
 # returns slider value when slider changes
-class s_value_holder:
-    def __init__(self):
-        self.s_value = 10
-        #print(val)
-    def slider_changed(self, val):
-        self.s_value = int(slider.get())
-    def get_variable(self):
-        print(self.s_value)
-        return self.s_value
 
 # Set up tkinter
 
@@ -47,16 +42,36 @@ study = dicom.DICOMStudy(dir)
 series_array = np.array(study.getSeries())
 # print(current_value)
 canvas = tk.Canvas(root,width=500,height=500)
+
+#some random stuff
+image=Image.open('1.27.24tower.jpg')
+image=ImageTk.PhotoImage(image.resize((300,300)))
+annotation_text=canvas.create_text(50,50,text='')
+a_button = Button(root, text='Annotate',command=annotate)
+a_button.pack()
+
 array = study.get(series_array[0])
 current_value = tk.IntVar()
+
+class s_value_holder:
+    def __init__(self):
+        self.s_value = 0
+        #print(val)
+    def slider_changed(self, val):
+        self.s_value = int(slider.get())
+        slice_cv_array = (array[indexval.get_variable()]).pixel_array
+        img = ImageTk.PhotoImage(image=Image.fromarray(slice_cv_array))
+        displayimg = canvas.create_image(0,0, anchor="nw", image=img)
+        canvas.itemconfig(displayimg, image=img)
+    def get_variable(self):
+        print(self.s_value)
+        return self.s_value
+
 indexval = s_value_holder()
 slider = ttk.Scale(master=root, length=200, from_=0, to=40, orient='horizontal', variable=current_value, command = indexval.slider_changed)
 
 
-slice_cv_array = (array[indexval.get_variable()]).pixel_array
-img = ImageTk.PhotoImage(image=Image.fromarray(slice_cv_array))
-displayimg = canvas.create_image(0,0, anchor="nw", image=img)
-canvas.itemconfig(displayimg, image=img)
+
 
 entry=tk.Entry()
 
